1e71ed7722b03c55f304f7fc31d1b56ca9acef39
BRANYA <tbd2504@gmail.com>
2023-12-20 12:15:46 +0200

    Add django-debug-tool-bar to settings

--- src/core/settings/environments/development.py
+++ src/core/settings/environments/development.py
@@ -1,5 +1,16 @@
 """
 Development setting file
 """
+from core.settings.components.base import INSTALLED_APPS, MIDDLEWARE
 
 DEBUG = True
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+MIDDLEWARE += [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+]
+
+INTERNAL_IPS = ['127.0.0.1']
