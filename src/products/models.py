7c9dd24c4bedddfe534158e3f04191ed7e1c1898
BRANYA <tbd2504@gmail.com>
2024-01-15 15:43:42 +0200

    Add Product model

--- src/products/models.py
+++ src/products/models.py
@@ -1,6 +1,28 @@
 from django.db import models
 
-from utils.mixins import UUIDMixin
+from utils.mixins import CreatedAndUpdatedDateTimeMixin, UUIDMixin
+
+
+class Stock(models.TextChoices):
+    IN_STOCK = 'in_stock', 'In stock'
+    OUT_OF_STOCK = 'out_of_stock', 'Out of stock'
+    TO_ORDER = 'to_order', 'To order'
+
+
+class Product(UUIDMixin, CreatedAndUpdatedDateTimeMixin):
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50, unique=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='products')
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.CharField(max_length=20, choices=Stock.choices, default=Stock.IN_STOCK)
+    description = models.TextField(blank=True, null=True)
+    is_displayed = models.BooleanField(default=True)
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return self.name
 
 
 class Category(UUIDMixin):
--- src/products/tests/test_models.py
+++ src/products/tests/test_models.py
@@ -1,5 +1,99 @@
-from products.models import Category
+from products.models import Category, Product, Stock
 from utils.tests import CustomTestCase
+
+
+class ProductModelTest(CustomTestCase):
+    def setUp(self) -> None:
+        self.model = Product
+
+    def test_model_has_necessary_fields(self):
+        necessary_fields = [
+            'uuid',
+            'name',
+            'slug',
+            'category',
+            'stock',
+            'price',
+            'description',
+            'is_displayed',
+            'updated',
+            'created',
+        ]
+        self.assertModelHasNecessaryFields(self.model, necessary_fields)
+
+    def test_uuid_field(self):
+        """
+        Tests:
+        uuid field is primary key;
+        """
+        field = self.get_model_field(self.model, 'uuid')
+        self.assertTrue(field.primary_key)
+
+    def test_name_field(self):
+        """
+        Tests:
+        name field has max length as 50;
+        """
+        field = self.get_model_field(self.model, 'name')
+        self.assertEqual(field.max_length, 50)
+
+    def test_slug_field(self):
+        """
+        Tests:
+        slug field has max length as 50;
+        slug field is unique;
+        """
+        field = self.get_model_field(self.model, 'slug')
+        self.assertEqual(field.max_length, 50)
+        self.assertTrue(field.unique)
+
+    def test_category_field(self):
+        """
+        Tests:
+        category field has relation many to one;
+        category field has Category as related model;
+        """
+        field = self.get_model_field(self.model, 'category')
+        self.assertTrue(field.many_to_one)
+        self.assertIs(field.related_model, Category)
+
+    def test_stock_field(self):
+        """
+        Tests:
+        stock field is Stock.IN_STOCK by default;
+        stock field has Stock.choices;
+        """
+        field = self.get_model_field(self.model, 'stock')
+        self.assertEqual(field.default, Stock.IN_STOCK)
+        self.assertEqual(field.choices, Stock.choices)
+
+    def test_price_field(self):
+        """
+        Tests:
+        price field has max digits as ten;
+        price field has decimal places as two;
+        """
+        field = self.get_model_field(self.model, 'price')
+        self.assertEqual(field.max_digits, 10)
+        self.assertEqual(field.decimal_places, 2)
+
+    def test_is_displayed_field(self):
+        """
+        Tests:
+        is_displayed field is true by default;
+        """
+        field = self.get_model_field(self.model, 'is_displayed')
+        self.assertTrue(field.default)
+
+    def test_description_field(self):
+        """
+        Tests:
+        description field can be null;
+        description field can be blank;
+        """
+        field = self.get_model_field(self.model, 'description')
+        self.assertTrue(field.null)
+        self.assertTrue(field.blank)
 
 
 class CategoryModelTest(CustomTestCase):
