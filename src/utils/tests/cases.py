41c271b25a8346bb18f63c846e36d1e44e4ccb9f
BRANYA <tbd2504@gmail.com>
2024-01-15 11:56:48 +0200

    Add APITestCase

---
+++ src/utils/tests/cases.py
@@ -0,0 +1,42 @@
+from typing import Type
+
+from django.db.models import Field, Model
+from rest_framework.serializers import Serializer
+from rest_framework.test import APITestCase
+
+
+class CustomTestCase(APITestCase):
+    @staticmethod
+    def get_meta_attr_of_model(model: Type[Model], name: str):
+        return getattr(model._meta, name)
+
+    @staticmethod
+    def get_model_field(model: Type[Model], name: str) -> Type[Field]:
+        return model._meta.get_field(name)
+
+    @staticmethod
+    def get_model_field_names(model: Type[Model]) -> list[str]:
+        return [field.name for field in model._meta.fields]
+
+    @staticmethod
+    def get_serializer_field(serializer: Type[Serializer], name: str) -> Type[Field]:
+        return serializer().fields[name]
+
+    @staticmethod
+    def get_serializer_field_names(serializer: Type[Serializer]) -> list[str]:
+        return list(serializer().fields)
+
+    def assertModelHasNecessaryFields(self, model: Type[Model], necessary_fields: list[str]):
+        model_fields = self.get_model_field_names(model)
+        model_fields.sort()
+        necessary_fields.sort()
+
+        for field in necessary_fields:
+            self.assertIn(field, model_fields)
+
+    def assertSerializerHasOnlyExpectedFields(self, serializer: Type[Serializer], expected_fields: list[str]):
+        serializer_fields = self.get_serializer_field_names(serializer)
+        serializer_fields.sort()
+        expected_fields.sort()
+
+        self.assertListEqual(serializer_fields, expected_fields)
