d819bb8e9f4d8a1c97f5265b436a1f1a69e3082a
BRANYA <tbd2504@gmail.com>
2024-01-15 13:49:47 +0200

    Add Category to admin

--- src/products/admin.py
+++ src/products/admin.py
@@ -1,1 +1,28 @@
-# Register your models here.
+from django.contrib import admin
+
+from products.models import Category
+
+
+class InlineCategory(admin.TabularInline):
+    model = Category
+    fields = ['uuid', 'name']
+    readonly_fields = ['uuid']
+    extra = 1
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'is_parent_category', 'is_sub_category']
+    fields = ['uuid', 'name', 'parent']
+    readonly_fields = ['uuid']
+    search_fields = ['name']
+    ordering = ['parent', 'name']
+    inlines = [InlineCategory]
+
+    @staticmethod
+    def is_parent_category(instance: Category):
+        return instance.is_parent_category
+
+    @staticmethod
+    def is_sub_category(instance: Category):
+        return instance.is_sub_category
