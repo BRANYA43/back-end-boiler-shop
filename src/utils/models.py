4ef95224c59e5111e58af6d2037f70662e82ac7b
BRANYA <tbd2504@gmail.com>
2024-01-16 18:27:09 +0200

    Add Image model

--- src/utils/models.py
+++ src/utils/models.py
@@ -1,6 +1,17 @@
 from django.db import models
 
-from utils.mixins import UUIDMixin
+from utils.mixins import CreatedAndUpdatedDateTimeMixin, UUIDMixin
+
+
+class Image(UUIDMixin, CreatedAndUpdatedDateTimeMixin):
+    name = models.SlugField(max_length=50, unique=True)
+    image = models.ImageField()
+
+    class Meta:
+        pass
+
+    def __str__(self):
+        return self.name
 
 
 class Attribute(UUIDMixin):
--- src/utils/tests/test_models.py
+++ src/utils/tests/test_models.py
@@ -1,5 +1,32 @@
-from utils.models import Attribute
+from utils.models import Attribute, Image
 from utils.tests import CustomTestCase
+
+
+class ImageModelTest(CustomTestCase):
+    def setUp(self) -> None:
+        self.model = Image
+
+    def test_model_has_necessary_fields(self):
+        necessary_fields = ['uuid', 'name', 'image', 'updated', 'created']
+        self.assertModelHasNecessaryFields(self.model, necessary_fields)
+
+    def test_uuid_field(self):
+        """
+        Tests:
+        uuid field is primary key;
+        """
+        field = self.get_model_field(self.model, 'uuid')
+        self.assertTrue(field.primary_key)
+
+    def test_name_field(self):
+        """
+        Tests:
+        name field has max length as 50;
+        name field is unique;
+        """
+        field = self.get_model_field(self.model, 'name')
+        self.assertEqual(field.max_length, 50)
+        self.assertTrue(field.unique)
 
 
 class AttributeModelTest(CustomTestCase):
