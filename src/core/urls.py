152ce1953834f2721278fd28c08c4fba65530dfe
BRANYA <tbd2504@gmail.com>
2024-01-16 15:45:22 +0200

    Add media root and ulr

--- src/core/urls.py
+++ src/core/urls.py
@@ -1,4 +1,5 @@
 from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
@@ -15,6 +16,8 @@ urlpatterns = [
     path('baton/', include('baton.urls')),
     path('api/', include(api_urls)),
 ]
+
+urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
