c5097ed16718ab515a96d1d25fcc429cd185cb49
BRANYA <tbd2504@gmail.com>
2024-01-16 19:55:57 +0200

    Add signal to delete image file for Image

---
+++ src/utils/tests/test_signals.py
@@ -0,0 +1,13 @@
+from django.core.files.uploadedfile import SimpleUploadedFile
+
+from utils.models import Image
+from utils.tests import CustomTestCase
+
+
+class DeleteImageFileSignalTest(CustomTestCase):
+    def setUp(self) -> None:
+        self.image = SimpleUploadedFile('test_image.png', b'', content_type='image/png')
+
+    def test_signal_deletes_image_file(self):
+        image = Image.objects.create(name='name', image=self.image)
+        image.delete()
