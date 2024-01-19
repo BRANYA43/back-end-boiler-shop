4db0bf51b4e830be110a28ac25e92ed30e515497
BRANYA <tbd2504@gmail.com>
2024-01-19 12:39:05 +0200

    Refactor typping for CustomTestCase

--- src/utils/tests/cases.py
+++ src/utils/tests/cases.py
@@ -1,32 +1,34 @@
 from typing import Type
 
-from django.db.models import Field, Model
-from rest_framework.serializers import Serializer
+from django.db import models
+from rest_framework import serializers
 from rest_framework.test import APITestCase
 
 
 class CustomTestCase(APITestCase):
     @staticmethod
-    def get_meta_attr_of_model(model: Type[Model], name: str):
+    def get_meta_attr_of_model(model: Type[models.Model], name: str):
         return getattr(model._meta, name)
 
     @staticmethod
-    def get_model_field(model: Type[Model], name: str) -> Type[Field]:
+    def get_model_field(model: Type[models.Model], name: str) -> Type[models.Field]:
         return model._meta.get_field(name)
 
     @staticmethod
-    def get_serializer_field(serializer: Type[Serializer], name: str) -> Type[Field]:
+    def get_serializer_field(serializer: Type[serializers.Serializer], name: str) -> Type[serializers.Field]:
         return serializer().fields[name]
 
     @staticmethod
-    def get_serializer_field_names(serializer: Type[Serializer]) -> list[str]:
+    def get_serializer_field_names(serializer: Type[serializers.Serializer]) -> list[str]:
         return list(serializer().fields)
 
-    def assertModelHasNecessaryFields(self, model: Type[Model], necessary_fields: list[str]):
+    def assertModelHasNecessaryFields(self, model: Type[models.Model], necessary_fields: list[str]):
         for field in necessary_fields:
             self.assertTrue(hasattr(model, field))
 
-    def assertSerializerHasOnlyExpectedFields(self, serializer: Type[Serializer], expected_fields: list[str]):
+    def assertSerializerHasOnlyExpectedFields(
+        self, serializer: Type[serializers.Serializer], expected_fields: list[str]
+    ):
         serializer_fields = self.get_serializer_field_names(serializer)
         serializer_fields.sort()
         expected_fields.sort()
