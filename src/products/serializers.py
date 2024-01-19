443ef9e181743da1de98ab089d237229281d73bb
BRANYA <tbd2504@gmail.com>
2024-01-19 13:18:22 +0200

    Add ProductSerializer

--- src/products/serializers.py
+++ src/products/serializers.py
@@ -1,6 +1,28 @@
 from rest_framework import serializers
 
-from products.models import Category
+from products.models import Category, Product
+
+
+class ProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Product
+        fields = [
+            'url',
+            'uuid',
+            'category',
+            'name',
+            'slug',
+            'price',
+            'stock',
+            'grade',
+            'description',
+            'is_displayed',
+            'specification',
+            'image_set',
+            'updated',
+            'created',
+        ]
+        read_only_fields = ['uuid', 'updated', 'created']
 
 
 class CategorySerializer(serializers.HyperlinkedModelSerializer):
--- src/products/tests/test_serializer.py
+++ src/products/tests/test_serializer.py
@@ -1,7 +1,60 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
 
-from products.serializers import CategorySerializer
+from products.serializers import CategorySerializer, ProductSerializer
 from utils.tests import CustomTestCase
+
+
+class ProductSerializerTest(CustomTestCase):
+    def setUp(self) -> None:
+        self.serializer = ProductSerializer
+
+    def test_serializer_inherit_necessary_classes(self):
+        necessary_classes = [HyperlinkedModelSerializer]
+        for class_ in necessary_classes:
+            self.assertTrue(issubclass(self.serializer, class_))
+
+    def test_serializer_has_only_expected_fields(self):
+        expected_fields = [
+            'url',
+            'uuid',
+            'category',
+            'name',
+            'slug',
+            'price',
+            'stock',
+            'grade',
+            'description',
+            'is_displayed',
+            'specification',
+            'image_set',
+            'updated',
+            'created',
+        ]
+        self.assertSerializerHasOnlyExpectedFields(self.serializer, expected_fields)
+
+    def test_uuid_field(self):
+        """
+        Tests:
+        field is read only;
+        """
+        field = self.get_serializer_field(self.serializer, 'uuid')
+        self.assertTrue(field.read_only)
+
+    def test_updated_field(self):
+        """
+        Tests:
+        field is read only;
+        """
+        field = self.get_serializer_field(self.serializer, 'updated')
+        self.assertTrue(field.read_only)
+
+    def test_created_field_is_read_only(self):
+        """
+        Tests:
+        field is read only;
+        """
+        field = self.get_serializer_field(self.serializer, 'created')
+        self.assertTrue(field.read_only)
 
 
 class CategorySerializerTest(CustomTestCase):
