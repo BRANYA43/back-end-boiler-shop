9ac2f0bc68f2028fb3f4f840c79e53d2804c4197
BRANYA <tbd2504@gmail.com>
2024-01-23 15:09:01 +0200

    Add Customer to admin

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,6 +1,18 @@
 from django.contrib import admin
 
-from orders.models import Order
+from orders.models import Order, Customer
+
+
+class CustomerInline(admin.StackedInline):
+    model = Customer
+    fields = ['full_name', 'email', 'phone']
+
+
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['order', 'full_name', 'email', 'phone']
+    fields = ['order', 'full_name', 'email', 'phone']
+    search_fields = ['full_name', 'email', 'phone']
 
 
 @admin.register(Order)
@@ -11,3 +23,4 @@ class OrderAdmin(admin.ModelAdmin):
     ordering = ['created']
     list_filter = ['status', 'delivery', 'payment', 'is_paid']
     search_fields = ['comment', 'delivery_address']
+    inlines = [CustomerInline]
