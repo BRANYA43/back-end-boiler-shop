17cf7d6283957f9e53e0827fab116aa447b80289
BRANYA <tbd2504@gmail.com>
2024-01-23 14:16:29 +0200

    Add Order to admin

--- src/orders/admin.py
+++ src/orders/admin.py
@@ -1,1 +1,13 @@
-# Register your models here.
+from django.contrib import admin
+
+from orders.models import Order
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['uuid', 'status', 'delivery', 'payment', 'is_paid', 'updated', 'created']
+    fields = ['uuid', 'status', 'payment', 'is_paid', 'delivery', 'delivery_address', 'updated', 'created']
+    readonly_fields = ['uuid', 'updated', 'created']
+    ordering = ['created']
+    list_filter = ['status', 'delivery', 'payment', 'is_paid']
+    search_fields = ['comment', 'delivery_address']
