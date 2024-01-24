edeecee2aa7f27572d5828e55abd68c626dc82fa
BRANYA <tbd2504@gmail.com>
2024-01-24 17:56:09 +0200

    Add OrderProductSerializer

--- src/orders/serializers.py
+++ src/orders/serializers.py
@@ -1,6 +1,13 @@
 from rest_framework import serializers
 
-from orders.models import Order
+from orders.models import Order, OrderProduct
+
+
+class OrderProductSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = OrderProduct
+        fields = ['url', 'uuid', 'order', 'product', 'quantity', 'price', 'total_cost']
+        read_only_fields = ['uuid', 'total_cost']
 
 
 class OrderSerializer(serializers.HyperlinkedModelSerializer):
--- src/orders/tests/test_serializers.py
+++ src/orders/tests/test_serializers.py
@@ -1,7 +1,37 @@
 from rest_framework.serializers import HyperlinkedModelSerializer
 
-from orders.serializers import OrderSerializer
+from orders.serializers import OrderSerializer, OrderProductSerializer
 from utils.tests import CustomTestCase
+
+
+class OrderProductSerializerTest(CustomTestCase):
+    def setUp(self) -> None:
+        self.serializer = OrderProductSerializer
+
+    def test_serializer_inherit_necessary_classes(self):
+        necessary_classes = [HyperlinkedModelSerializer]
+        for class_ in necessary_classes:
+            self.assertTrue(issubclass(self.serializer, class_))
+
+    def test_serializer_has_only_expected_fields(self):
+        expected_fields = ['url', 'uuid', 'order', 'product', 'quantity', 'price', 'total_cost']
+        self.assertSerializerHasOnlyExpectedFields(self.serializer, expected_fields)
+
+    def test_uuid_field(self):
+        """
+        Tests:
+        field is read only;
+        """
+        field = self.get_serializer_field(self.serializer, 'uuid')
+        self.assertTrue(field.read_only)
+
+    def test_total_cost_field(self):
+        """
+        Test:
+        field is read only;
+        """
+        field = self.get_serializer_field(self.serializer, 'total_cost')
+        self.assertTrue(field.read_only)
 
 
 class OrderSerializerTest(CustomTestCase):
