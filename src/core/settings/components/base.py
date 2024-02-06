bd0e56a20d7d13059ee9b4899d4ac2ed5bef7633
BRANYA <tbd2504@gmail.com>
2024-02-06 18:05:54 +0200

    Remove django-taggit

--- src/core/settings/components/base.py
+++ src/core/settings/components/base.py
@@ -17,7 +17,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # External apps
-    'taggit',
     'rest_framework',
     'drf_spectacular',
     # Internal apps
