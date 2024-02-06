4e52bd67e2a8bb96899bf214c844e15b089c7ef2
BRANYA <tbd2504@gmail.com>
2024-02-06 18:36:03 +0200

    Fix adding media root

--- src/core/urls.py
+++ src/core/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('api/', include(api_urls)),
 ]
 
-urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     urlpatterns += [
