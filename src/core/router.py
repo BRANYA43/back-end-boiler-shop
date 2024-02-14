33aacfafa2dffcd9248424c4c1989527553e5e9f
BRANYA <tbd2504@gmail.com>
2024-02-14 16:02:25 +0200

    Delete all serializer and views

--- src/core/router.py
+++ src/core/router.py
@@ -1,12 +1,7 @@
 from rest_framework.routers import DefaultRouter
-from orders import views as orders_views
 from products import views as product_views
 
 router = DefaultRouter()
 
 router.register(r'category', product_views.CategoryViewSet)
 router.register(r'product', product_views.ProductViewSet)
-
-router.register(r'order', orders_views.OrderViewSet)
-router.register(r'customer', orders_views.CustomerViewSet)
-router.register(r'order-product', orders_views.OrderProductViewSet)
