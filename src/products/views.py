f5d43d7f5ec9b3e115874d7684af803412fb46b7
BRANYA <tbd2504@gmail.com>
2024-02-14 14:14:32 +0200

    Add filters to Product

--- src/products/views.py
+++ src/products/views.py
@@ -1,6 +1,8 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 
 from products import serializers
+from products.filters import ProductFilter
 from products.models import Category, Product
 
 
@@ -10,6 +12,8 @@ class ProductViewSet(viewsets.ReadOnlyModelViewSet):
         'list': serializers.ProductListSerializer,
         'retrieve': serializers.ProductDetailSerializer,
     }
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     def get_serializer_class(self):
         return self.serializer_classes[self.action]
