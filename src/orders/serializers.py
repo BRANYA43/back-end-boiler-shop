4b48bf0e33523916424276ed705d8d0a455443a8
BRANYA <tbd2504@gmail.com>
2024-02-14 16:47:32 +0200

    Create serializer create of order

--- src/orders/serializers.py
+++ src/orders/serializers.py
@@ -1,1 +1,25 @@
+from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
+from django.utils.translation import gettext as _
+
+from orders.models import Order
+
+
+class OrderCreateSerializer(serializers.ModelSerializer):
+    default_error_messages = {
+        'invalid': _('Invalid data. Expected a dictionary, but got {datatype}.'),
+        'invalid_delivery_address': _('Delivery address cannot be empty if delivery way isn\'t "pickup".'),
+    }
+
+    class Meta:
+        model = Order
+        fields = ['uuid', 'delivery', 'delivery_address', 'payment', 'comment']
+        read_only_fields = ['uuid']
+
+    def validate_delivery(self, value):
+        code = 'invalid_delivery_address'
+        delivery = value
+        delivery_address = self.initial_data.get('delivery_address')
+        if delivery != Order.Delivery.PICKUP.lower() and not bool(delivery_address):
+            raise ValidationError(self.default_error_messages[code], code)
---
+++ src/orders/tests/test_serializers.py
@@ -0,0 +1,30 @@
+from rest_framework.serializers import ModelSerializer
+
+from orders import serializers
+from utils.tests import CustomTestCase
+
+
+class OrderCreateSerializerTest(CustomTestCase):
+    def setUp(self) -> None:
+        self.serializer_class = serializers.OrderCreateSerializer
+
+    def test_serializer_inherit_necessary_classes(self):
+        classes = [ModelSerializer]
+        for class_ in classes:
+            self.assertTrue(self.serializer_class, class_)
+
+    def test_serializer_has_only_expected_fields(self):
+        expected_fields = ['uuid', 'delivery', 'delivery_address', 'payment', 'comment']
+        self.assertSerializerHasOnlyExpectedFields(self.serializer_class, expected_fields)
+
+    def test_uuid_field_is_read_only(self):
+        field = self.get_serializer_field(self.serializer_class, 'uuid')
+        self.assertTrue(field.read_only)
+
+    def test_serializer_is_invalid_if_delivery_address_is_empty_when_delivery_is_different_from_pickup(self):
+        serializer = self.serializer_class(data={'delivery': 'nova_post'})
+
+        self.assertFalse(serializer.is_valid())
+
+        expected_error = self.serializer_class.default_error_messages['invalid_delivery_address']
+        self.assertEqual(serializer.errors['delivery'][0], expected_error)
