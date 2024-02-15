95e96baf187cb6a7f3e52a2675722a1a638a9a81
BRANYA <tbd2504@gmail.com>
2024-02-15 11:46:47 +0200

    Refactro model tests of products app

--- src/products/tests/test_models.py
+++ src/products/tests/test_models.py
@@ -5,13 +5,7 @@ from products.models import Category, Product, ProductImageSet, Specification, P
 from utils.mixins import CreatedAndUpdatedDateTimeMixin, ImageSetMixin, UUIDMixin
 from utils.models import Attribute, Image
 from utils.tests import CustomTestCase
-from utils.tests.creators import (
-    create_test_product,
-    create_test_price,
-    create_test_image,
-    create_test_category,
-    create_test_attribute,
-)
+from utils.tests import creators
 
 
 class PriceModelTest(CustomTestCase):
@@ -52,7 +46,7 @@ class PriceModelTest(CustomTestCase):
         self.assertTrue(field.auto_now_add)
 
     def test_instance_model_is_deleted_after_deleting_product(self):
-        price = create_test_price()
+        price = creators.create_test_price()
         product = price.product
 
         self.assertEqual(Price.objects.count(), 1)
@@ -84,14 +78,14 @@ class ProductImageSetModelTest(CustomTestCase):
     def test_model_is_created_after_creating_product(self):
         self.assertEqual(self.model.objects.count(), 0)
 
-        create_test_product()
+        creators.create_test_product()
 
         self.assertEqual(self.model.objects.count(), 1)
 
     def test_model_is_deleted_after_deleting_product(self):
         self.assertEqual(self.model.objects.count(), 0)
 
-        product = create_test_product()
+        product = creators.create_test_product()
 
         self.assertEqual(self.model.objects.count(), 1)
 
@@ -103,7 +97,7 @@ class ProductImageSetModelTest(CustomTestCase):
 class SpecificationModelTest(CustomTestCase):
     def setUp(self) -> None:
         self.model = Specification
-        self.attributes = [create_test_attribute() for i in range(10)]
+        self.attributes = [creators.create_test_attribute() for i in range(10)]
 
     def test_model_inherit_necessary_mixins(self):
         mixins = [UUIDMixin]
@@ -163,14 +157,14 @@ class SpecificationModelTest(CustomTestCase):
     def test_model_is_created_after_creating_product(self):
         self.assertEqual(Specification.objects.count(), 0)
 
-        create_test_product()
+        creators.create_test_product()
 
         self.assertEqual(Specification.objects.count(), 1)
 
     def test_model_is_deleted_after_deleting_product(self):
         self.assertEqual(Specification.objects.count(), 0)
 
-        product = create_test_product()
+        product = creators.create_test_product()
 
         self.assertEqual(Specification.objects.count(), 1)
 
@@ -179,7 +173,7 @@ class SpecificationModelTest(CustomTestCase):
         self.assertEqual(Specification.objects.count(), 0)
 
     def test_card_attributes_field_cannot_have_different_attributes_from_attributes_of_all_attributes_field(self):
-        specification = create_test_product().specification
+        specification = creators.create_test_product().specification
         specification.all_attributes.set(self.attributes[:3])
         specification.card_attributes.set(self.attributes[1:4])
 
@@ -187,7 +181,7 @@ class SpecificationModelTest(CustomTestCase):
             specification.full_clean()
 
     def test_card_attributes_field_cannot_have_more_3_attributes(self):
-        specification = create_test_product().specification
+        specification = creators.create_test_product().specification
         specification.all_attributes.set(self.attributes)
         specification.card_attributes.set(self.attributes[:4])
 
@@ -195,7 +189,7 @@ class SpecificationModelTest(CustomTestCase):
             specification.full_clean()
 
     def test_detail_attributes_field_cannot_have_different_attributes_from_attributes_of_all_attributes_field(self):
-        specification = create_test_product().specification
+        specification = creators.create_test_product().specification
         specification.all_attributes.set(self.attributes[:5])
         specification.card_attributes.set(self.attributes[1:5])
 
@@ -203,7 +197,7 @@ class SpecificationModelTest(CustomTestCase):
             specification.full_clean()
 
     def test_detail_attributes_field_cannot_have_more_5_attributes(self):
-        specification = create_test_product().specification
+        specification = creators.create_test_product().specification
         specification.all_attributes.set(self.attributes)
         specification.card_attributes.set(self.attributes[:6])
 
@@ -292,22 +286,22 @@ class ProductModelTest(CustomTestCase):
         self.assertTrue(field.blank)
 
     def test_price_property_returns_latest_created_price(self):
-        product = create_test_product()
-        price_1 = create_test_price(product, value=1000)
+        product = creators.create_test_product()
+        price_1 = creators.create_test_price(product, value=1000)
 
         self.assertEqual(price_1.value, product.price.value)
 
-        price_2 = create_test_price(product, value=2000)
+        price_2 = creators.create_test_price(product, value=2000)
 
         self.assertNotEqual(price_1.value, product.price.value)
         self.assertEqual(price_2.value, product.price.value)
 
     def test_price_property_returns_none_if_prices_is_empty(self):
-        product = create_test_product()
+        product = creators.create_test_product()
         self.assertIsNone(product.price)
 
     def test_model_allows_category_to_be_deleted(self):
-        product = create_test_product()
+        product = creators.create_test_product()
         category = product.category
 
         with self.assertRaises(ProtectedError):
@@ -405,8 +399,8 @@ class CategoryModelTest(CustomTestCase):
         self.assertFalse(parent.is_parent_category)
 
     def test_model_is_protected_from_deleting_image(self):
-        image = create_test_image()
-        create_test_category(image=image)
+        image = creators.create_test_image()
+        creators.create_test_category(image=image)
 
         with self.assertRaises(ProtectedError):
             image.delete()
