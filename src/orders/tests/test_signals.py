2d84e5f10dc4f69bdb39d9c6f4a6772e67e5d647
BRANYA <tbd2504@gmail.com>
2024-02-15 13:37:57 +0200

    Add changes of product price field to order product

--- src/orders/tests/test_signals.py
+++ src/orders/tests/test_signals.py
@@ -7,16 +7,16 @@ class SetOrderProductPriceSignalTest(CustomTestCase):
         product = create_test_product(price=1000)
         order_product = create_test_order_product(product=product)
 
-        self.assertEqual(order_product.price.value, product.price.value)
+        self.assertEqual(order_product.price.value, product.price)
 
     def test_signal_doesnt_set_price_before_saving_order_product_if_price_is_already_set(self):
         product_1 = create_test_product(price=1000)
         product_2 = create_test_product(price=2000)
         order_product = create_test_order_product(product=product_1)
 
-        self.assertEqual(order_product.price.value, product_1.price.value)
+        self.assertEqual(order_product.price.value, product_1.price)
 
         order_product.product = product_2
         order_product.save()
 
-        self.assertEqual(order_product.price.value, product_1.price.value)
+        self.assertEqual(order_product.price.value, product_1.price)
