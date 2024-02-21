bf7469ae8c50056a4b050aa0ab3453284602b475
BRANYA <tbd2504@gmail.com>
2024-02-21 13:02:58 +0200

    Change corsheaders settings

--- src/core/settings/components/corsheaders.py
+++ src/core/settings/components/corsheaders.py
@@ -1,9 +1,8 @@
 from core.settings.components.base import INSTALLED_APPS, MIDDLEWARE
-from core.settings import env
 
 INSTALLED_APPS += ['corsheaders']
 MIDDLEWARE += [
     'corsheaders.middleware.CorsMiddleware',
 ]
 
-CORS_ALLOWED_ORIGINS = env.get('CORS_ALLOWED_ORIGINS', '').split()
+CORS_ALLOW_ALL_ORIGINS = True
