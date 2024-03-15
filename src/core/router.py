8ad0846ec12b0f9458e813bcea69bc39ba23ed3d
BRANYA <tbd2504@gmail.com>
2024-03-15 18:37:26 +0200

    Remove views of orders app

--- src/core/router.py
+++ src/core/router.py
@@ -1,11 +1,7 @@
 from rest_framework.routers import DefaultRouter
 from products import views as product_views
-from orders import views as order_views
 
 router = DefaultRouter()
 
 router.register(r'category', product_views.CategoryViewSet)
 router.register(r'product', product_views.ProductViewSet)
-router.register(r'order', order_views.OrderViewSet)
-router.register(r'order-product', order_views.OrderProductViewSet)
-router.register(r'customer', order_views.CustomerViewSet)
