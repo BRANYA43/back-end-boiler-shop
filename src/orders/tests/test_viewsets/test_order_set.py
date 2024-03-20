aeead7cf5ef545c42206c6172722f285d1aeb362
BRANYA <tbd2504@gmail.com>
2024-03-20 16:41:23 +0200

    Add sending mail to customer and owner

--- src/orders/tests/test_viewsets/test_order_set.py
+++ src/orders/tests/test_viewsets/test_order_set.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core import mail
 from rest_framework import status
 from rest_framework.reverse import reverse
 
@@ -47,3 +49,18 @@ class OrderSetCreateViewTest(CustomTestCase):
         self.client.post(self.url, self.data, format='json')
 
         self.assertEqual(OrderProduct.objects.count(), 1)
+
+    def test_view_sends_two_msgs(self):
+        self.assertEqual(len(mail.outbox), 0)
+
+        self.client.post(self.url, self.data, format='json')
+
+        self.assertEqual(len(mail.outbox), 2)
+
+    def test_view_sends_msg_for_customer(self):
+        self.client.post(self.url, self.data, format='json')
+        self.assertTrue([msg for msg in mail.outbox if msg.to[0] == self.data['customer']['email']])
+
+    def test_view_sends_msg_for_owner(self):
+        self.client.post(self.url, self.data, format='json')
+        self.assertTrue([msg for msg in mail.outbox if msg.to[0] == settings.EMAIL_HOST_USER])
--- src/orders/views.py
+++ src/orders/views.py
@@ -1,5 +1,8 @@
+from django.conf import settings
+from django.template.loader import render_to_string
 from rest_framework import viewsets, status
 from rest_framework.response import Response
+from django.core.mail import EmailMultiAlternatives
 
 from orders import serializers
 
@@ -10,5 +13,41 @@ class OrderSetViewSet(viewsets.ViewSet):
     def create(self, request, *args, **kwargs):
         serializer = self.serializer_class(data=request.data)
         serializer.is_valid(raise_exception=True)
-        serializer.save()
+        data = serializer.save()
+        self.send_message_about_order(data)
         return Response(status=status.HTTP_201_CREATED)
+
+    def send_message_about_order(self, data):
+        self._send_msg_to_customer(data)
+        self._send_msg_to_owner(data)
+
+    def _send_msg_to_customer(self, data):
+        order = data['order']
+        customer = data['customer']
+        context = data
+        body = render_to_string('orders/msg_body_of_order_for_customer.html', context)
+
+        email = EmailMultiAlternatives(
+            subject=self._get_subject(order.uuid), body=body, from_email=settings.EMAIL_HOST_USER, to=[customer.email]
+        )
+
+        email.attach_alternative(body, 'text/html')
+        email.send()
+
+    def _send_msg_to_owner(self, data):
+        order = data['order']
+        context = data
+        body = render_to_string('orders/msg_body_of_order_for_owner.html', context)
+
+        email = EmailMultiAlternatives(
+            subject=self._get_subject(order.uuid),
+            body=body,
+            from_email=settings.EMAIL_HOST_USER,
+            to=[settings.EMAIL_HOST_USER],
+        )
+
+        email.attach_alternative(body, 'text/html')
+        email.send()
+
+    def _get_subject(self, order_uuid):
+        return f'Диво Комфорт | Замовлення №{order_uuid}'
